0bf6f8ee02b0a763b3f6eea2b0a6c1bf2ca5ae7c
Hyeonwoo Ok <ok-e_1004@hanmail.net>
2023-03-14 09:07:11 +0900

    second commit

--- main.js
+++ main.js
@@ -2,23 +2,71 @@ window.onload = function(){
     var canvas = document.getElementById("canvas1");
     var context = canvas.getContext("2d");
 
+    
+    var tableArr = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,0,0,0,0],
+        [0,1,1,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,1,0,0,0],
+        [0,0,0,0,0,0,1,1,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0]
+    ];
+
+    var block_type =[
+        [
+            [1,1],
+            [1,1]
+        ]
+    ]
+
     background();
     leftBox();
     scoreBox();
-
+    drawRect();
+    
+    
     function background(){
-        context.rect(50,30,600,600);
-        context.stroke();
+        
+    }
+
+    function drawRect(){
+        
+        for(var i = 0; i <tableArr.length; i++){
+            for(var j=0; j<tableArr[i].length; j++){
+                    if(tableArr[i][j]!==0){
+                        context.fillRect(j*30,i*30,30,30);
+                    }
+            }
+        }
     }
     
+    
+
+
+
     function leftBox(){
-        context.rect(60,40,180,100);
+        context.rect(20,10,140,100);
         context.stroke();
     };
     
     
     function scoreBox(){
-        context.rect(490,40,150,30);
+        context.rect(180,30,100,30);
+        context.fillText("스코어:",190,50);
         context.stroke();
     };
 
